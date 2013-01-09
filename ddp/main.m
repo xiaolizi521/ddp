702dfcd197335b79d4778fd43c422224d672ec05
AlvinLiu <liulei1513@gmail.com>
2013-01-09 10:28:28 +0800

    init

---
+++ ddp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ddp
+//
+//  Created by Coolin 006 on 13-1-9.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
